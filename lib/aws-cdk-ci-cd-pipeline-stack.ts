190ccecbdf28c5492242d058fec759ab3ad4feef
pabcrudel <pabcrudel@gmail.com>
2023-08-21 01:35:33 +0200

    Created a bucket by my own to solve deletion error

--- lib/aws-cdk-ci-cd-pipeline-stack.ts
+++ lib/aws-cdk-ci-cd-pipeline-stack.ts
@@ -1,20 +1,33 @@
 import * as cdk from 'aws-cdk-lib';
-import * as pipeline from 'aws-cdk-lib/pipelines';
+import * as pipelines from 'aws-cdk-lib/pipelines';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 export class AwsCdkCiCdPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new pipeline.CodePipeline(this, 'Pipeline', {
-      synth: new pipeline.ShellStep('Synth', {
-        input: pipeline.CodePipelineSource.gitHub('pabcrudel/aws-cdk-ci-cd-pipeline', 'main'),
+    /** The S3 Bucket to use for storing the pipeline's artifact. */
+    const artifactBucket = new s3.Bucket(this, 'ArtifactBucket', {
+      publicReadAccess: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      accessControl: s3.BucketAccessControl.PRIVATE,
+      objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+    });
+
+    new pipelines.CodePipeline(this, 'Pipeline', {
+      synth: new pipelines.ShellStep('Synth', {
+        input: pipelines.CodePipelineSource.gitHub('pabcrudel/aws-cdk-ci-cd-pipeline', 'main'),
         commands: [
           'npm ci',
           'npm run build',
           'npx cdk synth',
         ],
       }),
+      artifactBucket: artifactBucket,
     });
   };
 };
