0415c0ae14a7d7ae73def3117c2a7cd2afd92ee1
pabcrudel <pabcrudel@gmail.com>
2023-08-21 12:00:56 +0200

    Created a lambda function stack

---
+++ lib/lambda-stack.ts
@@ -0,0 +1,17 @@
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
+import * as path from 'path';
+
+export class LambdaStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, stageName: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        new lambda.Function(this, 'LambdaFunction', {
+            runtime: lambda.Runtime.NODEJS_18_X,
+            handler: 'handler.main',
+            code: lambda.Code.fromAsset(path.join(__dirname, 'lambda')),
+            environment: { 'stageName': stageName },
+        });
+    };
+};
